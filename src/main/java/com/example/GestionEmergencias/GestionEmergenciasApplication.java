f053d1d77e263d5ce052da82e811ea365859aa03
dxn1l <danielmorenorey2012@gmail.com>
2024-12-14 19:11:25 +0100

    Initial commit

---
+++ src/main/java/com/example/GestionEmergencias/GestionEmergenciasApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionEmergencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmergenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEmergenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestionEmergencias/GestionEmergenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionEmergencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmergenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
